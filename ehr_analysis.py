d2cf56f92dbca400503e3709eacd6153b887cee3
sdonatileach <sdona@DESKTOP-R9F496I.localdomain>
2022-01-31 23:13:09 -0500

    sick_patients function

--- ehr_analysis.py
+++ ehr_analysis.py
@@ -32,6 +32,10 @@ def parse_data(filename: str) -> dict:
 
 def num_older_than(age: float) -> int:
     """Return the number of patients older than a given age (in years)."""
+
+    data_dict = parse_data("/mnt/c/Users/sdona/Documents/Duke/22Spring"
+    "/821BIOSTAT/03Assignment/PatientCorePopulatedTable.txt")
+
     dates = data_dict["PatientDateOfBirth"]
     dates_2 = [datetime.datetime.strptime(i, "%Y-%m-%d %H:%M:%S.%f") for i in dates]
     today = datetime.datetime.today()
@@ -49,5 +53,18 @@ def num_older_than(age: float) -> int:
     return(len(older))
 
 
-def sick_patients(lab:str, gt_lt: str, value:float, ???) -> str:
-    """Return a (unique) list of patients who have a given test with value above (">") or below ("<") a given level."""
+def sick_patients(lab:str, gt_lt: str, value:float) -> str:
+    """Return a (unique) list of patients who have a given test with value 
+    above (">") or below ("<") a given level."""
+    
+    data_dict = parse_data("/mnt/c/Users/sdona/Documents/Duke/22Spring/"
+    "821BIOSTAT/03Assignment/LabsCorePopulatedTable.txt")
+    
+    values = []
+    for i in range(len(data_dict["PatientID"])):
+        for (key, value_pair) in data_dict.items():
+            if value_pair[i] == lab:
+                if eval(data_dict["LabValue"][i] + gt_lt + str(value)):
+                    if(data_dict["PatientID"][i] not in values):
+                        values.append(data_dict["PatientID"][i])
+    return(len(values))

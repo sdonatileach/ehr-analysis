a7b48536f0df3a4f8dc3ef83eefc51d06e7128e2
sdonatileach <sdona@DESKTOP-R9F496I.localdomain>
2022-02-13 21:53:52 -0500

    test suite

--- test_ehr.py
+++ test_ehr.py
@@ -1,44 +1,37 @@
 """Test EHR analysis."""
 import pytest
-from ehr_analysis import parse_data, create_data_dict, date_time, num_older_than, sick_patients, age_at_admis
-
-patient_dict = create_data_dict("Patients.txt")
-lab_dict = create_data_dict("Labs.txt")
+from ehr_analysis import parse_data, date_time, num_older_than, sick_patients, age_at_admis
 
 def test_parse_data():
     """Test parse_data()."""
-    # with pytest.raises(ValueError):
-    #     add("hello", 5)
-
-# patient_dict = parse_data(
-# "/mnt/c/Users/sdona/Documents/Duke/22Spring"
-# "/821BIOSTAT/03Assignment/PatientCorePopulatedTable.txt"
-# , "\t")
-# lab_dict = parse_data(
-# "/mnt/c/Users/sdona/Documents/Duke/22Spring"
-# "/821BIOSTAT/03Assignment/LabsCorePopulatedTable.txt"
-# , "\t")
+    assert len(parse_data("PatientCorePopulatedTable.txt", "\t")) == 7
+    assert len(parse_data("LabsCorePopulatedTable.txt", "\t")) == 6
 
 def test_date_time():
     """Test date_time()."""
-    pass
+    assert len(date_time(patient_dict["PatientDateOfBirth"])) == 100
+    assert len(date_time(lab_dict["LabDateTime"])) == 111483
 
 def test_num_older_than():
     """Test num_older_than()."""
-    pass
+    assert num_older_than(68, patient_dict) == 42
+    assert num_older_than(100.5, patient_dict) == 3
 
 def test_sick_patients():
     """Test sick_patients()."""
-    assert len(sick_patients(lab="METABOLIC: ALBUMIN", gt_lt=">", value= 5.9, lab_dict=lab_dict)) == 42
-    assert len(sick_patients(lab="URINALYSIS: WHITE BLOOD CELLS", gt_lt ="<", value=.5, lab_dict=lab_dict)) == 86
+    assert len(sick_patients("METABOLIC: ALBUMIN", ">",  5.9, lab_dict)) == 42
+    assert len(sick_patients("URINALYSIS: WHITE BLOOD CELLS", "<", .5, lab_dict)) == 86
     with pytest.raises(ValueError):
-        sick_patients(lab="hello", gt_lt=">", value=5, lab_dict=lab_dict)
+        sick_patients("hello", ">", 5, lab_dict)
     with pytest.raises(ValueError):
-        sick_patients(lab="METABOLIC: ALBUMIN", gt_lt="?", value= 5.9, lab_dict=lab_dict)
-    
-
+        sick_patients("METABOLIC: ALBUMIN", "?",  5.9, lab_dict)
 
 def test_age_at_admis():
     """Test age_at_admis()."""
     assert age_at_admis("03A481F5-B32A-4A91-BD42-43EB78FEBA77",lab_dict,patient_dict) == 20.8
-    pass
+    with pytest.raises(ValueError):
+        age_at_admis("42",lab_dict,patient_dict)
+
+
+patient_dict = parse_data("PatientCorePopulatedTable.txt","\t")
+lab_dict = parse_data("LabsCorePopulatedTable.txt","\t")
